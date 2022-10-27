1f2ad24af0145753c8c7e54659696df68f987f16
Josh Faigan <djjoker13@gmail.com>
2022-10-26 20:39:38 -0400

    added class to tests

--- tests.py
+++ tests.py
@@ -1,4 +1,42 @@
-#Tested using base class, all passed.
+import random
+
+class BankAccount:
+    def __init__(self, full_name,):
+        self.name = full_name
+        self.account = random.randint(80000000, 89999999)
+        self.balance = 0
+    # TODO: round out balances to not cause crazy decimals
+    def deposit(self, amount):
+        self.balance += amount
+        print(f"Amount deposited: ${amount} new balance: ${self.balance}")
+    
+    def withdraw(self, amount):
+        self.balance -= amount
+        if self.balance <0:
+            self.balance -= 10
+            print(f"Insufficient funds. You have been charged $10. New balance is: ${self.balance}")
+        else:
+            print(f"Amount withdrawn: ${amount} new balance: {self.balance}")
+    
+    def get_balance(self):
+        print(f"{self.name}, your balance is: ${self.balance}")
+        return self.balance
+    
+    def add_interest(self):
+        if self.balance >=0:
+            interest = self.balance * 0.00083
+            self.balance = self.balance + interest
+            self.balance = round(self.balance, 2)
+        else:
+            print('You can not earn interest on overdraft balances')
+    
+    def print_statement(self):
+        hide_account = str(self.account)
+        hide_account = (hide_account[4:8])
+        hide_account = f"****{hide_account}"
+        print('--------------------')
+        print(f"{self.name}\nAccount No.: {hide_account}\nBalance: ${self.balance}")
+        print('--------------------')
 
 mitchell_account = BankAccount('Mitchell Hudson')
 mitchell_account.deposit(400000)
